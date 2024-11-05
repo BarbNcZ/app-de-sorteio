97d95a7bb4d3fa8e0095e6c0613a5e27cbd30170
Barbara Niemietz <barb.niemietz@outlook.com>
2024-11-05 18:19:31 -0300

    Initial commit

---
+++ app/src/main/java/com/androidestudos/appdesorteio/MainActivity.java
@@ -0,0 +1,38 @@
+package com.androidestudos.appdesorteio;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void alterText(View view){
+
+        TextView text = findViewById(R.id.resultText);
+
+        int number = new Random().nextInt(11);
+
+        text.setText(" " + number);
+
+    }
+}
